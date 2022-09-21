49f8ff0cc6b4280a771431c586af04614270e8eb
Emmanuel <85338878+Okekejr@users.noreply.github.com>
2022-09-21 15:34:36 -0500

    styles, herotext and button, heroSection

---
+++ src/theme/Heading.ts
@@ -0,0 +1,25 @@
+export const Heading = {
+  baseStyle: {
+    color: "neutral.100",
+    fontWeight: "500",
+  },
+  sizes: {
+    "2xl": {
+      fontSize: {
+        base: "5rem",
+        lg: "4rem",
+      },
+    },
+    "4xl": {
+      fontSize: {
+        base: "2.5rem",
+        lg: "4rem",
+      },
+    },
+  },
+  variants: {
+    heavy: {
+      fontWeight: { base: "700", lg: "900" },
+    },
+  },
+}
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,14 +1,27 @@
-import { extendTheme } from "@chakra-ui/react"
-
+import { extendTheme, ThemeConfig } from "@chakra-ui/react"
 import { colors } from "./colors"
 import { styles } from "./styles"
+import { Heading } from "./Heading"
+import { fonts } from "./Fonts"
+import { fontSizes } from "./fontSizes"
+
+export const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+}
 
 const theme = extendTheme({
+  config,
   styles,
   sizes: {
     container: {
       xl: "1448px",
     },
+  },
+  fonts,
+  fontSizes,
+  components: {
+    Heading,
   },
   colors,
 })
